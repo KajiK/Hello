30d255ae836122a2758d5c739787a822900631fb
KajiK <kobito3_suika@yahoo.co.jp>
2017-05-11 04:00:37 +0900

    First commit.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Transform trans;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("Hello World!");
+		Debug.Log("これがUnityらしいです。ねむい。");
+
+		int    i  = 10;   //整数型
+		double d  = 1.0;  //実数型
+		bool   b  = true; //論理値型
+		string s = "str"; //文字列型
+		print(i); //出力:10
+		print(d); //出力:1
+		print(b); //出力:True
+		print(s); //出力:str
+
+		Debug.Log(trans.position);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
